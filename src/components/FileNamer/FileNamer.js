8d2f43f5a5984d275480ac92589f5284dff84341
lorenzoVwebDev <lorenzoviganego.work@libero.it>
2025-01-12 13:18:29 +0100

    updated

--- src/components/FileNamer/FileNamer.js
+++ src/components/FileNamer/FileNamer.js
@@ -3,14 +3,18 @@ import { useState } from 'react';
 function FileNamer() {
   const [name, setName] = useState('');
   const [alert, setAlert] = useState(false);
+  const [badCharacterAlert, setBadCharacterAlert] = useState(false);
 
   const validate = (name, event) => {
     if (/\*/.test(name)) {
       event.preventDefault();
+      setBadCharacterAlert(true)
       setAlert(true)
       return
     }
-      setAlert(false);
+    event.preventDefault()
+    setBadCharacterAlert(false);
+    setAlert(false)
   }
 
   return (
@@ -23,10 +27,30 @@ function FileNamer() {
           <p>Name: </p>
           <input autoComplete="off" type="text" name="name" onChange={(event) => {
             setName(event.target.value);
-          }}/>
+          }}
+          onFocus={()=> {
+            setAlert(true);
+          }}  
+          onBlur={(event)=> {   
+            setTimeout(() => {setAlert(false)}, 200)
+          }}
+          />
           <div>
-            <button onClick={(event) => validate(name, event)}>Save</button>
-            {alert && <div> Forbidden Character:*</div>}
+            <button 
+              onClick={(event) => {validate(name, event)
+                console.log('hello')}
+              }
+              onBlur={()=> {setBadCharacterAlert(false)}}  
+            >Save</button>
+            {alert && <div>
+                <span role='img' aria-label='allowed'>✅</span>
+                Alphanumeric Characters
+                <br />
+                <span role="img" aria-label="not-allowed">⛔️</span>*
+              </div>}
+            {badCharacterAlert && <div>
+                <span>You inserted a bad character ⚠️</span>
+              </div>}
           </div>
         </label>
       </form>
