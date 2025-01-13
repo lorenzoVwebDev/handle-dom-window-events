eedd196cf9be24a56757bf0db938bc46aa44c34f
lorenzoVwebDev <lorenzoviganego.work@libero.it>
2025-01-13 09:24:01 +0100

    updated

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <>
       <GitRepositoryHeader/>
-      <FileNamer/>
+      <div className="file-namer-container">
+        <FileNamer/>
+      </div>
     </>
   );
 }
--- src/components/FileNamer/FileNamer.js
+++ src/components/FileNamer/FileNamer.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function FileNamer() {
   const [name, setName] = useState('');
@@ -12,36 +12,54 @@ function FileNamer() {
       setAlert(true)
       return
     }
-    event.preventDefault()
     setBadCharacterAlert(false);
     setAlert(false)
   }
 
+  useEffect(() => {
+    const handleWindowClick = () => setAlert(false)
+    if (alert) {
+      window.addEventListener('click', () => {
+        handleWindowClick()
+      })
+    } else {
+      window.removeEventListener('click', handleWindowClick());
+    }
+  }, [alert, setAlert])
+
   return (
     <div className="file-namer-wrapper">
       <div className="preview">
-        <h2>Preview:</h2>
+        <h2>Insert Your File's Name Here</h2>
       </div>
-      <form>
+      <form method="post" onSubmit={(event) => {
+        event.preventDefault()
+        window.alert(`Thank You for requesting that file "${event.target[0].value}"`)
+      }}>
         <label>
           <p>Name: </p>
           <input autoComplete="off" type="text" name="name" onChange={(event) => {
             setName(event.target.value);
           }}
-          onFocus={()=> {
+/*           onFocus={()=> {
             setAlert(true);
           }}  
           onBlur={(event)=> {   
             setTimeout(() => {setAlert(false)}, 200)
-          }}
+          }} */
           />
-          <div>
+        </label>
+          <div className="information-wrapper">
             <button 
-              onClick={(event) => {validate(name, event)
-                console.log('hello')}
-              }
-              onBlur={()=> {setBadCharacterAlert(false)}}  
-            >Save</button>
+              className="information"
+              onClick={(event)=> {
+                event.stopPropagation();
+                setAlert(true);
+              }}
+              type="button"
+            >
+              more information
+            </button>
             {alert && <div>
                 <span role='img' aria-label='allowed'>✅</span>
                 Alphanumeric Characters
@@ -52,7 +70,10 @@ function FileNamer() {
                 <span>You inserted a bad character ⚠️</span>
               </div>}
           </div>
-        </label>
+        <button 
+            onClick={(event) => {validate(name, event)}}
+            onBlur={()=> {setBadCharacterAlert(false)}}  
+        >Save</button>
       </form>
     </div>
   )
